adbb6e85b7f0684d041ce426516504bed9d75c32
Sigurður Snær Eiríksson <sse1987@gmail.com>
2015-04-01 07:28:17 +0000

    Added side scroll for each section.

--- html5-boilerplate/js/main.js
+++ html5-boilerplate/js/main.js
@@ -1,11 +1,19 @@
 
+// Set true too console log debug messages
+var verbose = false;
 
 var Info = function () {
 	
-	console.log('Constructor HERE!');
+	if (verbose) console.log('Constructor HERE!');
 	
 	// Private variables
 	var privateVar = "This is only to show example of a private variable";
+	var defaultSettings = {
+		quietPeriod: 500,
+		animationDuration: 1000,
+		easing: 'cubic-bezier(.7,.1,.3,.9)',
+		sectionVerticalMoveDistance: 0.5
+	};
 	
 	// Public variables
 	this.headerHeight = $('body > header').height();
@@ -13,21 +21,25 @@ var Info = function () {
 	this.sectionHeight = this.windowHeight - this.headerHeight;
 	this.sectionsEl = $('.main-content section');
 	this.footerPos = $('.main-content section').last().position().top + this.sectionHeight;
+	this.windowWidth = $(window).width();
 	
+	// High priority bug: If user refresh on mid site, has to init the active section
+	this.activeSection = this.sectionsEl.first();
+	this.activeSectionViewportY = 0;
 	
 	
 	// Initialization
 	this.init = function () {
-		console.log('init() HERE!');
+		if (verbose) console.log('init() BEGINS!');
 		
 		this.overwriteScroll();
 		this.bindResize();
 		this.bindKeydown();
 		
+		if (verbose) console.log('init() ENDS!');
 	};
 	
 	this.bindResize = function () {
-		console.log('bindResize() HERE! [sectionHeight:' + this.sectionHeight + ']');
 		var base = this;
 		
 		// Change section height dynamically
@@ -37,6 +49,7 @@ var Info = function () {
 			base.sectionHeight = base.windowHeight - base.headerHeight;
 			base.sectionsEl.css({ 'height': base.sectionHeight });
 			base.footerPos = $('.main-content section').last().position().top + base.sectionHeight;
+			this.windowWidth = $(window).width();
 		});
 		
 		// Initial the section height
@@ -50,12 +63,26 @@ var Info = function () {
 			var tag = e.target.tagName.toLowerCase();
 			switch (e.which) {
 				
+				case Key.HOME:
+					e.preventDefault();
+					animateTo({ position: 0 });
+				break;
+				
+				case Key.END:
+					e.preventDefault();
+					animateTo({ position: base.footerPos });
+				break;
+				
+				case Key.J:
+				case Key.W:
 				case Key.UP:
 				case Key.PAGE_UP:
 					e.preventDefault();
 					if (tag != 'input' && tag != 'textarea') base.moveUp();
 				break;
 				
+				case Key.K:
+				case Key.S:
 				case Key.DOWN:
 				case Key.PAGE_DOWN:
 				case Key.SPACE:
@@ -63,17 +90,18 @@ var Info = function () {
 					if (tag != 'input' && tag != 'textarea') base.moveDown();
 				break;
 				
-				case Key.HOME:
+				case Key.A:
+				case Key.LEFT:
 					e.preventDefault();
-					console.log('animateTo: 0');
-					animateTo({ position: 0 });
+					if (tag != 'input' && tag != 'textarea') base.moveLeft();
 				break;
 				
-				case Key.END:
+				case Key.D:
+				case Key.RIGHT:
 					e.preventDefault();
-					console.log('animateTo: ' + base.footerPos);
-					animateTo({ position: base.footerPos });
+					if (tag != 'input' && tag != 'textarea') base.moveRight();
 				break;
+				
 			}
 		});
 		
@@ -83,19 +111,17 @@ var Info = function () {
 		var base = this;
 		
 		var lastAnimation = 0;
-		var quietPeriod = 500;
-		var animationTime = 1000;
 		var newScrollMethod = function (event, delta) {
-			deltaOfInterest = delta;
-			//console.log('delta: ' + delta);
 			var timeNow = new Date().getTime();
 			// Cancel scroll if currently animating or within quiet period
-			if (timeNow - lastAnimation < quietPeriod + animationTime) {
+			var timeDiff = timeNow - lastAnimation;
+			var timeMin = defaultSettings.quietPeriod + defaultSettings.animationDuration;
+			if (timeDiff < timeMin) {
 				event.preventDefault();
 				return;
 			}
 			
-			if (deltaOfInterest < 0) {
+			if (delta < 0) {
 				base.moveDown();
 			} else {
 				base.moveUp();
@@ -111,6 +137,37 @@ var Info = function () {
 		
 	};
 	
+	
+	this.moveLeft = function () {
+		this.moveWithinActiveSection('left');
+	};
+	this.moveRight = function () {
+		this.moveWithinActiveSection('right');
+	};
+	this.moveWithinActiveSection = function (direction) {
+		//console.log('moveWithinActiveSection(' + direction + '); HERE!');
+		var base = this;
+		
+		if (!base.activeSection) return;
+		
+		var activeSectionWidth = $(base.activeSection).width();
+		//console.log('activeSectionWidth:' + activeSectionWidth + ' - windowWidth:' + base.windowWidth);
+		if (activeSectionWidth > base.windowWidth) {
+			
+			base.activeSectionViewportY += (direction === 'right' ? 1 : -1) * base.windowWidth * defaultSettings.sectionVerticalMoveDistance;
+			if (base.activeSectionViewportY < 0) {
+				base.activeSectionViewportY = 0;
+			} else if (base.activeSectionViewportY > (activeSectionWidth - base.windowWidth)) {
+				base.activeSectionViewportY = activeSectionWidth - base.windowWidth;
+			}
+			
+			$(base.activeSection).css('transform', 'translate3d(' + (-1 * base.activeSectionViewportY) + 'px,0,0)');
+			
+		}
+		
+	};
+	
+	
 	this.moveDown = function () {
 		this.moveToNextSection('down');
 	};
@@ -118,7 +175,7 @@ var Info = function () {
 		this.moveToNextSection('up');
 	};
 	this.moveToNextSection = function (direction) {
-		console.log('moveToNextSection(' + direction + '); HERE!');
+		//console.log('moveToNextSection(' + direction + '); HERE!');
 		var base = this;
 		
 		var offset = base.headerHeight;
@@ -126,15 +183,16 @@ var Info = function () {
 		//console.log('#### mainWindowTop: ' + mainWindowTop);
 		
 		var targetPos = mainWindowTop;
+		var thisSectionPos = 0;
 		if (direction === 'down') {
 			for (var i = 0; (targetPos == mainWindowTop && i < base.sectionsEl.length); i++) {
 				
-				var thisSectionPos = $(base.sectionsEl[i]).position().top;
+				thisSectionPos = $(base.sectionsEl[i]).position().top;
 				if (targetPos < thisSectionPos) {
 					targetPos = thisSectionPos;
+					this.activeSection = base.sectionsEl[i];
 				}
 				//console.log('     thisSectionPos[' + i + ']: ' + thisSectionPos + ' - targetPos: ' + targetPos);
-				
 			}
 			// This is a hack to be able to scroll to bottom
 			if (targetPos === mainWindowTop) {
@@ -142,14 +200,14 @@ var Info = function () {
 			}
 		}
 		else if (direction === 'up') {
-			for (var i = base.sectionsEl.length - 1; (targetPos == mainWindowTop && i >= 0); i--) {
+			for (var k = base.sectionsEl.length - 1; (targetPos == mainWindowTop && k >= 0); k--) {
 				
-				var thisSectionPos = $(base.sectionsEl[i]).position().top;
+				thisSectionPos = $(base.sectionsEl[k]).position().top;
 				if (targetPos > thisSectionPos) {
 					targetPos = thisSectionPos;
+					this.activeSection = base.sectionsEl[k];
 				}
 				//console.log('     thisSectionPos[' + i + ']: ' + thisSectionPos + ' - targetPos: ' + targetPos);
-				
 			}
 		}
 		
@@ -161,7 +219,7 @@ var Info = function () {
 
 $(document).ready(function () {
 	
-	console.log('document is ready!');
+	if (verbose) console.log('document is ready!');
 	
 	setTimeout(function() {
 		var info = new Info();
@@ -187,15 +245,15 @@ var animateTo = function (options) {
 	}
 	
 	// The default settings, if defined in options, it will overwrite the defaults
-	var defaults = {
+	var animateToDefaults = {
 		position: 0,
-		duration: 400,// 'fast' === 400ms && 'slow' === 1000ms
-		easing: 'swing',
+		duration: 1000,// 'fast' === 400ms && 'slow' === 1000ms
+		easing: 'cubic-bezier(.7,.1,.3,.9)',
 		callback: function () {}
 	};
 	
 	// Merge defaults and options, without modifying defaults
-	var settings = $.extend({}, defaults, options);
+	var settings = $.extend({}, animateToDefaults, options);
 	
 	// Executing scrolling animation to the position
 	// 'body, html' is to make sure it will work in all browsers
@@ -211,31 +269,51 @@ var animateTo = function (options) {
 	
 };
 
+// http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
 var Key = {
+	BACKSPACE: 8,
 	TAB: 9,
 	ENTER: 13,
+	SHIFT: 16,
+	CTRL: 17,
+	ALT: 18,
+	BREAK: 19,
+	PAUSE: 19,
+	CAPS_LOCK: 20,
 	ESC: 27,
 	SPACE: 32,
+	PAGE_UP: 33,
+	PAGE_DOWN: 34,
+	END: 35,
+	HOME: 36,
 	LEFT: 37,
 	UP: 38,
 	RIGHT: 39,
 	DOWN: 40,
-	SHIFT: 16,
-	CTRL: 17,
-	ALT: 18,
-	PAGE_UP: 33,
-	PAGE_DOWN: 34,
-	HOME: 36,
-	END: 35,
-	BACKSPACE: 8,
+	INSERT: 45,
 	DELETE: 46,
+	n0: 48, n1: 49, n2: 50, n3: 51, n4: 52,
+	n5: 53, n6: 54, n7: 55, n8: 56, n9: 57,
+	A: 65, B: 66, C: 67, D: 68, E: 69, F: 70,
+	G: 71, H: 72, I: 73, J: 74, K: 75, L: 76,
+	M: 77, N: 78, O: 79, P: 80, Q: 81, R: 82,
+	S: 83, T: 84, U: 85, V: 86, W: 87, X: 88,
+	Y: 89, Z: 90,
+	MULTIPLY: 106,
+	ADD: 107,
+	SUBTRACT: 108,
+	DECIMAL_POINT: 109,
+	DIVIDE: 110,
+	f1: 112, f2: 113, f3: 114, f4: 115,
+	f5: 116, f6: 117, f7: 118, f8: 119,
+	f9: 120, f10: 121, f11: 122, f12: 123,
 	isArrow: function (k) {
 		k = k.which ? k.which : k;
 		switch (k) {
-			case KEY.LEFT:
-			case KEY.RIGHT:
-			case KEY.UP:
-			case KEY.DOWN:
+			case Key.LEFT:
+			case Key.RIGHT:
+			case Key.UP:
+			case Key.DOWN:
 				return true;
 		}
 		return false;
@@ -243,9 +321,9 @@ var Key = {
 	isControl: function (e) {
 		var k = e.which;
 		switch (k) {
-			case KEY.SHIFT:
-			case KEY.CTRL:
-			case KEY.ALT:
+			case Key.SHIFT:
+			case Key.CTRL:
+			case Key.ALT:
 				return true;
 		}
 		if (e.metaKey) return true;
--- html5-boilerplate/js/sections/green.js
+++ html5-boilerplate/js/sections/green.js
@@ -11,10 +11,47 @@ $(document).ready(function () {
 	$('.main-content').append(base);
 	
 	
-	$('<h1>Hello, this is the green section</h1>').appendTo(base);
-	$('<p>This text is appended to the section by a different way.</p>').appendTo(base);
+	$('<p class="box" style="left: 50px;top: 200px;">Zombie art</p>').appendTo(base);
 	
-	// ...
+	$('<div></div>', {
+		'class': 'zombie',
+		'style': 'left: 150px;bottom: 10%;background-image: url("img/zombies/zombie-01.jpg");'
+	}).appendTo(base);
+	
+	$('<div></div>', {
+		'class': 'zombie',
+		'style': 'left: 550px;top: 10%;background-image: url("img/zombies/zombie-02.jpg");'
+	}).appendTo(base);
+	
+	$('<div></div>', {
+		'class': 'zombie',
+		'style': 'left: 940px;bottom: 5%;background-image: url("img/zombies/zombie-03.jpg");'
+	}).appendTo(base);
+	
+	$('<div></div>', {
+		'class': 'zombie',
+		'style': 'left: 1330px;top: 5%;background-image: url("img/zombies/zombie-04.jpg");'
+	}).appendTo(base);
+	
+	$('<div></div>', {
+		'class': 'zombie',
+		'style': 'left: 1725px;bottom: 20%;background-image: url("img/zombies/zombie-05.jpg");'
+	}).appendTo(base);
+	
+	$('<div></div>', {
+		'class': 'zombie',
+		'style': 'left: 2110px;top: 15%;background-image: url("img/zombies/zombie-06.jpg");'
+	}).appendTo(base);
+	
+	var max = 0;
+	var maxWidth = 0;
+	base.find('> *').each(function (i) {
+		if (max < $(this).position().left) {
+			max = $(this).position().left;
+			maxWidth = $(this).width();
+		}
+	});
+	base.css('width', (max + maxWidth));
 	
 });
 
