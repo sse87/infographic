d8aca7163d97151aea1647225030194dd3e4ce1d
kobblander <kobblander@gmail.com>
2015-04-01 14:22:41 +0000

    Grunt should be fixed now xD

---
+++ html5-boilerplate/Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+    'use strict';
+    // Project configuration
+    grunt.initConfig({
+        // Metadata
+        pkg: grunt.file.readJSON('package.json'),
+        // Task configuration
+        jshint: {
+            files: ['Gruntfile.js', 'js/main.js', 'js/sections/*.js'],
+            options: {
+                node: true,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                eqnull: true,
+                browser: true,
+                globals: { jQuery: true },
+                boss: true,
+                jquery: true
+            },
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.files %>',
+                tasks: ['jshint']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task
+    grunt.registerTask('default', ['jshint','watch']);
+};
