e8ce82b67db7b04571849d73f15db09e29386063
Sigurður Snær Eiríksson <sse1987@gmail.com>
2015-04-08 07:10:34 +0000

    Added findClosestSection to init correct section

--- html5-boilerplate/js/info.js
+++ html5-boilerplate/js/info.js
@@ -25,7 +25,7 @@ var Info = function () {
 	
 	// High priority bug: If user refresh on mid site, has to init the active section
 	this.activeSection = this.sectionsEl.first();
-	this.activeSectionViewportY = 0;
+	this.activeSectionViewportX = 0;
 	
 	this.progress = new Progress();
 	
@@ -43,6 +43,9 @@ var Info = function () {
 		this.bindKeydown();
 		
 		this.progress.init();
+		
+		this.activeSection = this.findClosestSection();
+		this.animateHeaderColor();
 		
 		if (verbose) { console.log('init() ENDS!'); }
 	};
@@ -178,21 +181,21 @@ var Info = function () {
 		//console.log('activeSectionWidth:' + activeSectionWidth + ' - windowWidth:' + base.windowWidth);
 		if (activeSectionWidth > base.windowWidth) {
 			
-			base.activeSectionViewportY += (direction === 'right' ? 1 : -1) * base.windowWidth * defaultSettings.sectionVerticalMoveDistance;
-			//console.log('if (' + base.activeSectionViewportY + ' < 0): ' + (base.activeSectionViewportY < 0).toString());
-			//console.log('else if (' + base.activeSectionViewportY + ' > ' + (activeSectionWidth - base.windowWidth) + '): ' + (base.activeSectionViewportY > (activeSectionWidth - base.windowWidth).toString()));
-			if (base.activeSectionViewportY < 0) {
-				base.activeSectionViewportY = 0;
-			} else if (base.activeSectionViewportY > (activeSectionWidth - base.windowWidth)) {
-				base.activeSectionViewportY = activeSectionWidth - base.windowWidth;
+			base.activeSectionViewportX += (direction === 'right' ? 1 : -1) * base.windowWidth * defaultSettings.sectionVerticalMoveDistance;
+			//console.log('if (' + base.activeSectionViewportX + ' < 0): ' + (base.activeSectionViewportX < 0).toString());
+			//console.log('else if (' + base.activeSectionViewportX + ' > ' + (activeSectionWidth - base.windowWidth) + '): ' + (base.activeSectionViewportX > (activeSectionWidth - base.windowWidth).toString()));
+			if (base.activeSectionViewportX < 0) {
+				base.activeSectionViewportX = 0;
+			} else if (base.activeSectionViewportX > (activeSectionWidth - base.windowWidth)) {
+				base.activeSectionViewportX = activeSectionWidth - base.windowWidth;
 			}
 			
 			// Move section view
-			$(base.activeSection).css('transform', 'translate3d(' + (-1 * base.activeSectionViewportY) + 'px,0,0)');
+			$(base.activeSection).css('transform', 'translate3d(' + (-1 * base.activeSectionViewportX) + 'px,0,0)');
 			
 			// Calculate the active section progress
 			var maxProgress = activeSectionWidth - base.windowWidth;
-			this.progress.setProgress( base.activeSectionViewportY / maxProgress );
+			this.progress.setProgress( base.activeSectionViewportX / maxProgress );
 			
 		}
 		
@@ -204,6 +207,47 @@ var Info = function () {
 	};
 	this.moveUp = function () {
 		this.moveToNextSection('up');
+	};
+	// This function can be used to get upper above section and below section and replace core function of moveToNextSection
+	this.findClosestSection = function () {
+		var base = this;
+		
+		// Calculate mainWindowTop and remember to include the offset (headerHeight)
+		var mainWindowTop = $(document).scrollTop() + base.headerHeight;
+		// Set the three value to help with the search
+		var indexBefore = -1, indexAfter = -1, currIndex = -1;
+		base.sectionsEl.each(function (i, section) {
+			var thisSectionPos = $(section).position().top;
+			if (mainWindowTop > thisSectionPos) {
+				indexBefore = i;
+			} else if (thisSectionPos === mainWindowTop) {
+				currIndex = i;
+			} else if (mainWindowTop < thisSectionPos && indexAfter === -1) {
+				indexAfter = i;
+			}
+		});
+		// After this loop if the currIndex is not set then we can find out which one is closest,
+		// the above section border or the below section border.
+		
+		// If currIndex is not set, calculate closer index and set that to current
+		if (currIndex === -1 && indexBefore !== indexAfter) {
+			// If for some reason one of them is not set, then choose the other
+			if (indexBefore === -1 || indexAfter === -1) {
+				// This math formula will give the one that has value
+				currIndex = indexBefore + indexAfter + 1;
+			} else {
+				// Calculate difference
+				var diffAbove = Math.abs( mainWindowTop - $(base.sectionsEl[indexBefore]).position().top );
+				var diffBelow = Math.abs( mainWindowTop - $(base.sectionsEl[indexAfter]).position().top );
+				// If above distance is smaller (closer) or equal, then choose above
+				if (diffAbove < diffBelow || diffAbove === diffBelow) {
+					currIndex = indexBefore;
+				} else if (diffAbove > diffBelow) {
+					currIndex = indexAfter;
+				}
+			}
+		}
+		return base.sectionsEl[currIndex];
 	};
 	this.moveToNextSection = function (direction) {
 		//console.log('moveToNextSection(' + direction + '); HERE!');
