8f35636e0343b3f4ce4226e9242a37917030c368
kobblander <kobblander@gmail.com>
2015-04-13 21:17:47 +0000

    Work on yellow

--- html5-boilerplate/js/sections/yellow.js
+++ html5-boilerplate/js/sections/yellow.js
@@ -15,23 +15,27 @@ $(document).ready(function () {
         }).appendTo(base);
 
 	$('<h1 class="yellow-title">Welcome back, survivor.</h1>').appendTo(wrapper);
-    $(  '<div class="chart-container">' + 
-            '<div class="chart-title">' + 
-                '<span>Causes of death</span>' + 
+    $(  
+        '<div class="chart-body">' + 
+            '<div class="chart-container">' + 
+                '<div class="chart-title">' + 
+                    '<span>Causes of death</span>' + 
+                '</div>' + 
+                '<div class="chart-wrapper chart-wrapper-bar">' + 
+                    '<canvas id="chart-bar-1"></canvas>' + 
+                '</div>' + 
             '</div>' + 
-            '<div class="chart-wrapper chart-wrapper-bar">' + 
-                '<canvas id="chart-bar-1"></canvas>' + 
+            '<div class="chart-container chart-container-pie">' + 
+                '<div class="chart-title">' + 
+                    '<span>Deaths by continent</span>' + 
+                '</div>' + 
+                '<div class="chart-wrapper chart-wrapper-pie">' + 
+                    '<canvas id="chart-pie-1"></canvas>' + 
+                '</div>' + 
+                '<div id="legend"></div>' + 
             '</div>' + 
-        '</div>').appendTo(wrapper);
-    $(  '<div class="chart-container chart-container-pie">' + 
-            '<div class="chart-title">' + 
-                '<span>Deaths by continent</span>' + 
-            '</div>' + 
-            '<div class="chart-wrapper chart-wrapper-pie">' + 
-                '<canvas id="chart-pie-1"></canvas>' + 
-            '</div>' + 
-        '<div id="legend"></div>' + 
-        '</div>').appendTo(wrapper);
+        '</div>'
+        ).appendTo(wrapper);
 
 });
 
