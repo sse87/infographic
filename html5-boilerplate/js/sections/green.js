ac6560adcefc4d0b8ed2acef62b7e9aa4697a960
kobblander <kobblander@gmail.com>
2015-04-14 02:38:01 +0000

    Zombies more cooler xD

--- html5-boilerplate/js/sections/green.js
+++ html5-boilerplate/js/sections/green.js
@@ -14,18 +14,55 @@ $(document).ready(function () {
         'class': 'limit limit-body' 
         }).appendTo(base);
 
+	var zombieBodyLeft = $(
+        '<div class="zombie-body zombie-body-left">' +
+            '<div class="zombie-title">' + 
+                '<h3>Sleepers</h3>' + 
+            '</div>' +
+        '</div>'
+    ).appendTo(wrapper);
+	var zombieBodyRight = $(
+        '<div class="zombie-body zombie-body-right">' + 
+            '<div class="zombie-title">' + 
+                '<h3>Walkers</h3>' + 
+            '</div>' +
+        '</div>'
+        ).appendTo(wrapper);
+	var zombieBodyMiddle = $(
+        '<div class="zombie-body zombie-body-middle">' + 
+            '<div class="zombie-title">' + 
+                '<h3>Runners</h3>' + 
+            '</div>' +
+        '</div>'
+        ).appendTo(wrapper);
+
+	var zombieWrapperLeft = $(
+        '<div class="zombie-wrapper">' + 
+        '</div>'
+        ).appendTo(zombieBodyLeft);
+
+	var zombieWrapperRight = $(
+        '<div class="zombie-wrapper">' + 
+        '</div>'
+        ).appendTo(zombieBodyRight);
+
+	var zombieWrapperMiddle = $(
+        '<div class="zombie-wrapper">' + 
+        '</div>'
+        ).appendTo(zombieBodyMiddle);
+
 	$('<div></div>', {
 		'class': 'zombie',
 		'style': 'background-image: url("img/zombies/zombie-01.png");'
-	}).appendTo(wrapper);
-	$('<div></div>', {
-		'class': 'zombie',
-		'style': 'background-image: url("img/zombies/zombie-02.png");'
-	}).appendTo(wrapper);
+	}).appendTo(zombieWrapperLeft);
 	$('<div></div>', {
 		'class': 'zombie',
 		'style': 'background-image: url("img/zombies/zombie-03.png");'
-	}).appendTo(wrapper);
+	}).appendTo(zombieWrapperRight);
+	$('<div></div>', {
+		'class': 'zombie',
+		'style': 'background-image: url("img/zombies/zombie-02.png");'
+	}).appendTo(zombieWrapperMiddle);
 	
 	
 	
